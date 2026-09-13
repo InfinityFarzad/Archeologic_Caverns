2c8e8566482d860425ccc17c2f25ab114f1d2e62
krisp <mangocatandmore@gmail.com>
2026-09-13 01:48:57 -0400

    NOT GAY ENOUGH

--- src/main/java/org/architectum_workshop/archeologic_caverns/common/init/ACItems.java
+++ src/main/java/org/architectum_workshop/archeologic_caverns/common/init/ACItems.java
@@ -20,7 +20,7 @@ public interface ACItems {
 
     Item DRIPSTONE_DRILL = register("dripstone_drill", DripstoneDrillItem::new, new Item.Properties());
     Item AMBER = register("amber", Item::new, new Item.Properties());
-    Item ITEM_OF_TRUE_GAYNESS = register("item_of_true_gayness", ItemOfTrueGaynessItem::new,  new Item.Properties().component(ChiseledLibComponents.ITEM_HIGHLIGHT, new ItemHighlightComponent(0xFFb38ef3, true)));
+    Item ITEM_OF_TRUE_GAYNESS = register("item_of_true_gayness", ItemOfTrueGaynessItem::new,  new Item.Properties().component(ChiseledLibComponents.ITEM_HIGHLIGHT, new ItemHighlightComponent(0xFFb38df3, true)));
 
     static <T extends Item> T register(String name, Function<Item.Properties, T> factory, Item.Properties itemProperties) {
         ResourceKey<Item> resourceKey = ResourceKey.create(Registries.ITEM, ArcheologicCaverns.id(name));
